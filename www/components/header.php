4f824a70151d2a28bd0af5d67383d4d3aff9e9f5
Michael Kasper <michael.kasper@ozprinciple.com>
2017-06-24 18:32:07 -0700

    made links relative incase the vhost is not setup correctly

--- www/components/header.php
+++ www/components/header.php
@@ -3,7 +3,7 @@
     <?php if ($config['showNav']): ?>
         <ul class="nav nav-pills float-right">
             <li class="nav-item">
-                <a class="nav-link active" href="/">Logout</a>
+                <a class="nav-link active" href="index.php">Logout</a>
             </li>
         </ul>
     <?php endif; ?>
@@ -14,12 +14,12 @@
 
         <ul class="nav nav-tabs">
             <li class="nav-item">
-                <a class="nav-link <?= $config['moviesActive'] ? 'active' : ''; ?>" href="/movies.php?<?= buildQueryString([], [
+                <a class="nav-link <?= $config['moviesActive'] ? 'active' : ''; ?>" href="movies.php?<?= buildQueryString([], [
                     'm', 'p', 'r'
                 ]) ?>">Movies</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link <?= $config['professionalsActive'] ? 'active' : ''; ?>" href="/professionals.php?<?= buildQueryString([], [
+                <a class="nav-link <?= $config['professionalsActive'] ? 'active' : ''; ?>" href="professionals.php?<?= buildQueryString([], [
                     'm', 'p', 'r'
                 ]) ?>">Professionals</a>
             </li>
